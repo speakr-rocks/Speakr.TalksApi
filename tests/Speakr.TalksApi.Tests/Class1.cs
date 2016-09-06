7b3db43834274eebddd6f6432e7a92d1c5a33c6c
jplebre <jplebre@gmail.com>
2016-09-06 23:45:25 +0100

    Add unit test project

---
+++ tests/Speakr.TalksApi.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Speakr.TalksApi.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
