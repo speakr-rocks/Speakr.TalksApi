0d7b643883d67784e7b51eab48f0a4c10c6eea4d
jplebre <jplebre@gmail.com>
2016-09-17 00:17:58 +0100

    clean up unused "usings"

--- src/Speakr.TalksApi/Models/FeedbackForm/Question.cs
+++ src/Speakr.TalksApi/Models/FeedbackForm/Question.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Speakr.TalksApi.Models.FeedbackForm
+﻿namespace Speakr.TalksApi.Models.FeedbackForm
 {
     public class Question
     {
